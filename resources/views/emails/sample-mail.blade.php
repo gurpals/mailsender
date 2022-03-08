4789540bfde2e999685ff09f3bf46eb0afa13162
gurpals <gurpal5686@gmail.com>
2022-03-08 14:59:57 +0530

    mail content changes

--- resources/views/emails/sample-mail.blade.php
+++ resources/views/emails/sample-mail.blade.php
@@ -1,11 +1,13 @@
 @component('mail::message')
 Hello {{ $contact->name }},
 
-Welcome to Negls!
+Congratulations for purchasing {{ $contact->domain }} domain!
 
-This is the testing {{ $contact->domain }} domain!
+I am from WIS support team. I can help you to build your website/app.
 
+Please feel free to contact me. I will guide you further for the same.
 
 Thanks & Regards,
-Negls
+
+Kindu!
 @endcomponent
